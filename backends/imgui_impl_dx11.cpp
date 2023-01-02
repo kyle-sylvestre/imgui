1ec6ec2b615f0cf1abc07b709e7eef8c5b9af49b
kyle-sylvestre <kyles3133@gmail.com>
2023-01-01 22:45:53 -0500

    win7 remove dependency on D3DCompiler_XX.dll

--- backends/imgui_impl_dx11.cpp
+++ backends/imgui_impl_dx11.cpp
@@ -35,10 +35,14 @@
 // DirectX
 #include <stdio.h>
 #include <d3d11.h>
+
+//#define COMPILE_SHADER_FROM_TEXT // run above win7, d3dcompile.dll is required
+#if defined(_MSC_VER) && defined(COMPILE_SHADER_FROM_TEXT)
 #include <d3dcompiler.h>
-#ifdef _MSC_VER
+#include <vector>   // hold the blobs
 #pragma comment(lib, "d3dcompiler") // Automatically link with d3dcompiler.lib as we are using D3DCompile() below.
 #endif
+
 
 // DirectX11 data
 struct ImGui_ImplDX11_Data
@@ -381,6 +385,7 @@ bool    ImGui_ImplDX11_CreateDeviceObjects()
 
     // Create the vertex shader
     {
+#if defined(COMPILE_SHADER_FROM_TEXT)
         static const char* vertexShader =
             "cbuffer vertexBuffer : register(b0) \
             {\
@@ -409,12 +414,23 @@ bool    ImGui_ImplDX11_CreateDeviceObjects()
               return output;\
             }";
 
-        ID3DBlob* vertexShaderBlob;
+        ID3DBlob* vertexShaderBlob = 0;
         if (FAILED(D3DCompile(vertexShader, strlen(vertexShader), NULL, NULL, NULL, "main", "vs_4_0", 0, 0, &vertexShaderBlob, NULL)))
             return false; // NB: Pass ID3DBlob* pErrorBlob to D3DCompile() to get error showing in (const char*)pErrorBlob->GetBufferPointer(). Make sure to Release() the blob!
-        if (bd->pd3dDevice->CreateVertexShader(vertexShaderBlob->GetBufferPointer(), vertexShaderBlob->GetBufferSize(), NULL, &bd->pVertexShader) != S_OK)
+
+        std::vector<uint8> bytes;
+        uint8 *vsb = (uint8 *)vertexShaderBlob->GetBufferPointer();
+        bytes.insert(bytes.begin(), vsb, vsb + vertexShaderBlob->GetBufferSize());
+        vertexShaderBlob->Release();
+        unsigned char *vert_shader = &bytes[0];
+        size_t vert_shader_bytecount = bytes.size();
+#else
+        unsigned char vert_shader[] = { 68,88,66,67,122,84,132,150,223,209,158,33,253,133,134,61,40,209,3,174,1,0,0,0,108,3,0,0,5,0,0,0,52,0,0,0,4,1,0,0,116,1,0,0,232,1,0,0,240,2,0,0,82,68,69,70,200,0,0,0,1,0,0,0,76,0,0,0,1,0,0,0,28,0,0,0,0,4,254,255,0,1,0,0,160,0,0,0,60,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,118,101,114,116,101,120,66,117,102,102,101,114,0,171,171,171,60,0,0,0,1,0,0,0,100,0,0,0,64,0,0,0,0,0,0,0,0,0,0,0,124,0,0,0,0,0,0,0,64,0,0,0,2,0,0,0,144,0,0,0,0,0,0,0,80,114,111,106,101,99,116,105,111,110,77,97,116,114,105,120,0,171,171,171,3,0,3,0,4,0,4,0,0,0,0,0,0,0,0,0,77,105,99,114,111,115,111,102,116,32,40,82,41,32,72,76,83,76,32,83,104,97,100,101,114,32,67,111,109,112,105,108,101,114,32,49,48,46,49,0,73,83,71,78,104,0,0,0,3,0,0,0,8,0,0,0,80,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,3,3,0,0,89,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,1,0,0,0,15,15,0,0,95,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,2,0,0,0,3,3,0,0,80,79,83,73,84,73,79,78,0,67,79,76,79,82,0,84,69,88,67,79,79,82,68,0,79,83,71,78,108,0,0,0,3,0,0,0,8,0,0,0,80,0,0,0,0,0,0,0,1,0,0,0,3,0,0,0,0,0,0,0,15,0,0,0,92,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,1,0,0,0,15,0,0,0,98,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,2,0,0,0,3,12,0,0,83,86,95,80,79,83,73,84,73,79,78,0,67,79,76,79,82,0,84,69,88,67,79,79,82,68,0,171,83,72,68,82,0,1,0,0,64,0,1,0,64,0,0,0,89,0,0,4,70,142,32,0,0,0,0,0,4,0,0,0,95,0,0,3,50,16,16,0,0,0,0,0,95,0,0,3,242,16,16,0,1,0,0,0,95,0,0,3,50,16,16,0,2,0,0,0,103,0,0,4,242,32,16,0,0,0,0,0,1,0,0,0,101,0,0,3,242,32,16,0,1,0,0,0,101,0,0,3,50,32,16,0,2,0,0,0,104,0,0,2,1,0,0,0,56,0,0,8,242,0,16,0,0,0,0,0,86,21,16,0,0,0,0,0,70,142,32,0,0,0,0,0,1,0,0,0,50,0,0,10,242,0,16,0,0,0,0,0,70,142,32,0,0,0,0,0,0,0,0,0,6,16,16,0,0,0,0,0,70,14,16,0,0,0,0,0,0,0,0,8,242,32,16,0,0,0,0,0,70,14,16,0,0,0,0,0,70,142,32,0,0,0,0,0,3,0,0,0,54,0,0,5,242,32,16,0,1,0,0,0,70,30,16,0,1,0,0,0,54,0,0,5,50,32,16,0,2,0,0,0,70,16,16,0,2,0,0,0,62,0,0,1,83,84,65,84,116,0,0,0,6,0,0,0,1,0,0,0,0,0,0,0,6,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, };
+        size_t vert_shader_bytecount = sizeof(vert_shader);
+#endif
+        if (bd->pd3dDevice->CreateVertexShader(vert_shader, vert_shader_bytecount, NULL, &bd->pVertexShader) != S_OK)
         {
-            vertexShaderBlob->Release();
+            //vertexShaderBlob->Release();
             return false;
         }
 
@@ -425,12 +441,12 @@ bool    ImGui_ImplDX11_CreateDeviceObjects()
             { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,   0, (UINT)IM_OFFSETOF(ImDrawVert, uv),  D3D11_INPUT_PER_VERTEX_DATA, 0 },
             { "COLOR",    0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, (UINT)IM_OFFSETOF(ImDrawVert, col), D3D11_INPUT_PER_VERTEX_DATA, 0 },
         };
-        if (bd->pd3dDevice->CreateInputLayout(local_layout, 3, vertexShaderBlob->GetBufferPointer(), vertexShaderBlob->GetBufferSize(), &bd->pInputLayout) != S_OK)
+        if (bd->pd3dDevice->CreateInputLayout(local_layout, 3, vert_shader, vert_shader_bytecount, &bd->pInputLayout) != S_OK)
         {
-            vertexShaderBlob->Release();
+            //vertexShaderBlob->Release();
             return false;
         }
-        vertexShaderBlob->Release();
+        //vertexShaderBlob->Release();
 
         // Create the constant buffer
         {
@@ -446,6 +462,7 @@ bool    ImGui_ImplDX11_CreateDeviceObjects()
 
     // Create the pixel shader
     {
+#if defined(COMPILE_SHADER_FROM_TEXT)
         static const char* pixelShader =
             "struct PS_INPUT\
             {\
@@ -462,15 +479,25 @@ bool    ImGui_ImplDX11_CreateDeviceObjects()
             return out_col; \
             }";
 
-        ID3DBlob* pixelShaderBlob;
+        ID3DBlob* pixelShaderBlob = 0;
         if (FAILED(D3DCompile(pixelShader, strlen(pixelShader), NULL, NULL, NULL, "main", "ps_4_0", 0, 0, &pixelShaderBlob, NULL)))
             return false; // NB: Pass ID3DBlob* pErrorBlob to D3DCompile() to get error showing in (const char*)pErrorBlob->GetBufferPointer(). Make sure to Release() the blob!
-        if (bd->pd3dDevice->CreatePixelShader(pixelShaderBlob->GetBufferPointer(), pixelShaderBlob->GetBufferSize(), NULL, &bd->pPixelShader) != S_OK)
+
+        std::vector<uint8> bytes;
+        uint8 *psb = (uint8 *)pixelShaderBlob->GetBufferPointer();
+        bytes.insert(bytes.begin(), psb, psb + pixelShaderBlob->GetBufferSize());
+        pixelShaderBlob->Release();
+        unsigned char *pixel_shader = &bytes[0];
+        size_t pixel_shader_bytecount = bytes.size();
+#else
+        unsigned char pixel_shader[] = { 68,88,66,67,158,206,133,114,167,151,82,180,109,196,40,250,16,192,210,193,1,0,0,0,148,2,0,0,5,0,0,0,52,0,0,0,212,0,0,0,72,1,0,0,124,1,0,0,24,2,0,0,82,68,69,70,152,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,28,0,0,0,0,4,255,255,0,1,0,0,110,0,0,0,92,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,101,0,0,0,2,0,0,0,5,0,0,0,4,0,0,0,255,255,255,255,0,0,0,0,1,0,0,0,12,0,0,0,115,97,109,112,108,101,114,48,0,116,101,120,116,117,114,101,48,0,77,105,99,114,111,115,111,102,116,32,40,82,41,32,72,76,83,76,32,83,104,97,100,101,114,32,67,111,109,112,105,108,101,114,32,49,48,46,49,0,171,171,73,83,71,78,108,0,0,0,3,0,0,0,8,0,0,0,80,0,0,0,0,0,0,0,1,0,0,0,3,0,0,0,0,0,0,0,15,0,0,0,92,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,1,0,0,0,15,15,0,0,98,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,2,0,0,0,3,3,0,0,83,86,95,80,79,83,73,84,73,79,78,0,67,79,76,79,82,0,84,69,88,67,79,79,82,68,0,171,79,83,71,78,44,0,0,0,1,0,0,0,8,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,15,0,0,0,83,86,95,84,97,114,103,101,116,0,171,171,83,72,68,82,148,0,0,0,64,0,0,0,37,0,0,0,90,0,0,3,0,96,16,0,0,0,0,0,88,24,0,4,0,112,16,0,0,0,0,0,85,85,0,0,98,16,0,3,242,16,16,0,1,0,0,0,98,16,0,3,50,16,16,0,2,0,0,0,101,0,0,3,242,32,16,0,0,0,0,0,104,0,0,2,1,0,0,0,69,0,0,9,242,0,16,0,0,0,0,0,70,16,16,0,2,0,0,0,70,126,16,0,0,0,0,0,0,96,16,0,0,0,0,0,56,0,0,7,242,32,16,0,0,0,0,0,70,14,16,0,0,0,0,0,70,30,16,0,1,0,0,0,62,0,0,1,83,84,65,84,116,0,0,0,3,0,0,0,1,0,0,0,0,0,0,0,3,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, };
+        size_t pixel_shader_bytecount = sizeof(pixel_shader);
+#endif
+
+        if (bd->pd3dDevice->CreatePixelShader(pixel_shader, pixel_shader_bytecount, NULL, &bd->pPixelShader) != S_OK)
         {
-            pixelShaderBlob->Release();
             return false;
         }
-        pixelShaderBlob->Release();
     }
 
     // Create the blending setup
