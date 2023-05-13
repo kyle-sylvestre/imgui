ab3a437b7a188506a10aa6df2540811e3b9b7904
kyle-sylvestre <kyles3133@gmail.com>
2023-05-13 13:02:34 -0400

    cast Texture pointer to ImTextureID

--- backends/imgui_impl_gdi.cpp
+++ backends/imgui_impl_gdi.cpp
@@ -722,8 +722,8 @@ void bind_imgui_painting()
 	uint8_t* tex_data;
 	int font_width, font_height;
 	io.Fonts->GetTexDataAsAlpha8(&tex_data, &font_width, &font_height);
-	const auto texture = new Texture{tex_data, font_width, font_height};
-	io.Fonts->TexID = texture;
+	Texture *texture = new Texture{tex_data, font_width, font_height};
+	io.Fonts->TexID = (ImTextureID)texture;
 }
 
 static Stats s_stats; // TODO: pass as an argument?
